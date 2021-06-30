c7404a9ede792176bd0231a7c6558d658c8a7dea
Ivens Diego Muller <ivensdiegomuller@gmail.com>
2021-06-30 19:57:02 -0300

    Create signup screen

--- users/routes/index.js
+++ users/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'Express' });
+});
+
+
 module.exports = router;
