16922b7bf4225e819701ad4fa395683b9991753e
Ivens Diego Muller <ivensdiegomuller@gmail.com>
2021-06-30 20:41:41 -0300

    Add axios

---
+++ users/public/javascripts/signup.js
@@ -0,0 +1,30 @@
+async function onSubmitSignUp(event) {
+    event.preventDefault();
+    const user = document.getElementById("userSignup").value;
+    const password = document.getElementById("passwordSignup").value;
+    /*
+        VALIDAR O FORMULÁRIO
+    */
+
+    // const response = await fetch("http://localhost:3000/users", {
+    //     method: 'POST',
+    //     body: JSON.stringify({
+    //         email: user,
+    //         password
+    //     }),
+    //     headers: {
+    //         'Content-Type': 'application/json', 
+    //     }
+    // });
+    // const data = await response.json();
+    const { data } = await axios.post("http://localhost:3000/users", {
+        email: user,
+        password
+    });
+    window.alert(`Usuário cadastrado com id ${data.id}`);
+}
+
+window.onload = function() {
+    const form = document.getElementById("formSignup");
+    form.addEventListener("submit", onSubmitSignUp);
+}
