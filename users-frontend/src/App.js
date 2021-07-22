0b7a5f25da1956bf1deeb4cf93d63aacfada714e
Ivens Diego Muller <ivensdiegomuller@gmail.com>
2021-07-21 21:47:36 -0300

    Add route wrapper

--- users-frontend/src/App.js
+++ users-frontend/src/App.js
@@ -4,6 +4,14 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Login from './pages/login';
 import SignUp from './pages/signUp';
 
+import RouteWrapper from './routes/RouteWrapper';
+
+/*
+  - Identificar que o usuário está logado
+  - Verificar se o usuário tem acesso em uma determinada rota (privada)
+  - Se não estiver logado, redirecionar para a página de login
+*/
+
 function App() {
   return (
     <BrowserRouter>
@@ -19,6 +27,10 @@ function App() {
         <Route exact path="/">
           <span>Eu sou a página home</span>
         </Route>
+
+        <RouteWrapper exact path="/privada" isPrivate component={() => (
+          <span>Essa página só pode ser acessada por um usuário logado</span>
+        )}/>
 
         <Route exact path="/404">
           Página não encontrada
